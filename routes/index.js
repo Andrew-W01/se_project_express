9b884481e3c0eb2b1965f78475599e9f85950a98
Andrew.Worley <drewjw1212@gmail.com>
2024-09-04 17:47:45 -0400

    project 12

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 
 router.use("/users", userRouter);
-router.use("./items", itemRouter);
+router.use("/items", itemRouter);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Resource not found" });
