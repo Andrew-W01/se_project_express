7efb475aade3b5162eb02d9c59a466e16c8a346e
Andrew.Worley <drewjw1212@gmail.com>
2024-09-15 18:55:57 -0400

    project 13

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const auth = require("./middlewares/auth");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -19,6 +20,7 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(auth);
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {
